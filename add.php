0411516addd0a41b44e57f5b970380e7b332dad2
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-04-03 21:51:57 +0300

    heroku

--- add.php
+++ add.php
@@ -5,5 +5,5 @@ if (!empty($_POST['text']) && !empty($_POST['name'])) {
     $mysqli->query(
         "INSERT INTO tabl VALUES (NULL, '$_POST[text]', '$_POST[name]')"
     );
-    header('Location: table.php');
+    header('Location: index.php');
 }
