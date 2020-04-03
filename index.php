7ed0121158503e55a5b125000305943dda10c3bd
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-04-03 23:40:25 +0300

    vf

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     $result = $mysqli->query('SELECT * FROM `tabl`');
 
     while ($row = $result->fetch_object()) {
-        echo "<b>($row->text)</b> <i>$row->name</i></br>";
+        echo "<b>". smile($row->text)."</b> <i>$row->name</i></br>";
     }   //вытягивает строки в виде объекта
 
     
