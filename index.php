6d3e3c806bfca2cb13c5754d648ba99487d8c91d
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-04-07 20:01:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,13 +23,13 @@ Port: 3306 -->
     include('config.php');
     include('conect.php');
     include('smile.php');
-    
 
-    $result_count = $mysqli->query('SELECT count(*) FROM `table`');
+
+    $result_count = $mysqli->query('SELECT count(*) FROM `table`'); //считаем количество строк в таблице
     $count = $result_count->fetch_array(MYSQLI_NUM)[0];
-    // echo "Количество строк: <b>$count</b><br>";
+    echo "количество записей: <b>$count</b>";
     $result_count->free();
-
+    // echo $pagesize;
 
     $pagecount = ceil($count / $pagesize);
 
@@ -37,25 +37,32 @@ Port: 3306 -->
 
     $startrow = ($currientpage - 1) * $pagesize;
 
-    $pageination = "<div class='padeination'>";
+    $pageination = "<div class='pageination'>";
 
     for ($i = 1; $i <= $pagecount; $i++) {
         $pageination .= "<a href='?page=$i'>$i</a>";
     }
-    $pageination = "</div>";
+    $pageination .= "</div>";
 
-    $result = $mysqli->query('SELECT * FROM `table` LIMIT $startrow, $pagesize');
+    $result = $mysqli->query("SELECT * FROM `table` LIMIT $startrow, $pagesize");
 
     echo $pageination;
 
+    echo "<table border='1'>\n";
     while ($row = $result->fetch_object()) {
         echo "<tr>";
-        echo "<td><b>" . smile($row->text) . "</b> <i>$row->name</i><br></td>";
+        echo "<td>" . smile($row->text) . "</td>";
+        echo "<td>" . $row->name . "</td>";
         echo "</tr>";
     }
+    echo "</table>\n";
+
+    echo $pageination;
+
     $result->free();
 
     $mysqli->close();
+    ?>
     ?>
     <form action="add.php" method="POST">
         <textarea name="text" cols="30" rows="10"></textarea> <br>
